2b518b20d95cf3d0d5cab0c7f21a01e51f7ea2eb
Arif R Gilang <arif17003@mail.unpad.ac.id>
2018-12-13 12:18:13 +0700

    finish wiring bot GoManabu, re-arrange code

--- app/src/main/java/com/rz/gomanabu/MainActivity.java
+++ app/src/main/java/com/rz/gomanabu/MainActivity.java
@@ -39,7 +39,6 @@ public class MainActivity extends AppCompatActivity {
     private ListView mListView;
     private EditText mEditTextMessage;
     private ImageButton mButtonSend;
-//    private ImageView mImageView;
     private ChatMessageAdapter mAdapter;
 
     @Override
@@ -50,13 +49,14 @@ public class MainActivity extends AppCompatActivity {
         wireView();
         setSendButtonListener();
         handleFileStream();
+        generateBot();
     }
 
     private void wireView(){
         mListView = findViewById(R.id.listView);
         mButtonSend = findViewById(R.id.btn_send);
         mEditTextMessage = findViewById(R.id.et_message);
-//        mImageView = findViewById(R.id.iv_image);
+
         mAdapter = new ChatMessageAdapter(this, new ArrayList<ChatMessage>());
         mListView.setAdapter(mAdapter);
     }
@@ -81,57 +81,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void handleFileStream(){
-        //checking SD card availability
-        boolean a = isSDCARDAvailable();
         //receiving the assets from the app directory
         AssetManager assets = getResources().getAssets();
-        File jayDir = new File(getCacheDir().toString() + "/hari/bots/Hari");
-        System.out.println("ini rip" + Environment.getExternalStorageDirectory().toString());
-        boolean b = jayDir.mkdirs();
+        File jayDir = new File(getCacheDir().toString() + "/gomanabu/bots/GoManabu");
+        boolean b = jayDir.mkdirs(); //create dir to be copied to cache dir
         if (jayDir.exists()) {
             //Reading the file
             try {
-                for (String dir : assets.list("Hari")) {
+                for (String dir : assets.list("GoManabu")) {
                     File subdir = new File(jayDir.getPath() + "/" + dir);
                     boolean subdir_check = subdir.mkdirs();
-                    for (String file : assets.list("Hari/" + dir)) {
+                    for (String file : assets.list("GoManabu/" + dir)) {
                         File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
                         if (f.exists()) {
                             continue;
                         }
                         InputStream in = null;
                         OutputStream out = null;
-                        in = assets.open("Hari/" + dir + "/" + file);
+                        in = assets.open("GoManabu/" + dir + "/" + file);
                         out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
-                        //copy file from assets to the mobile's SD card or any secondary memory
+                        // copy file to cache dir
                         copyFile(in, out);
                         in.close();
-                        in = null;
                         out.flush();
                         out.close();
-                        out = null;
                     }
                 }
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
+    }
+
+    private void generateBot(){
         //get the working directory
-        MagicStrings.root_path = getCacheDir().toString() + "/hari";
+        MagicStrings.root_path = getCacheDir().toString() + "/gomanabu";
         System.out.println("Working Directory = " + MagicStrings.root_path);
         AIMLProcessor.extension =  new PCAIMLProcessorExtension();
         //Assign the AIML files to bot for processing
-        bot = new Bot("Hari", MagicStrings.root_path, "chat");
+        bot = new Bot("GoManabu", MagicStrings.root_path, "chat");
         chat = new Chat(bot);
-//        String[] args = null;
-//        mainFunction(args);
     }
 
     private void sendMessage(String message) {
         ChatMessage chatMessage = new ChatMessage(message, true, false);
         mAdapter.add(chatMessage);
-        //respond as Helloworld
-//        mimicOtherMessage("HelloWorld");
     }
 
     private void mimicOtherMessage(String message) {
@@ -151,11 +145,6 @@ public class MainActivity extends AppCompatActivity {
         mAdapter.add(chatMessage);
     }
 
-    //check SD card availability
-    public static boolean isSDCARDAvailable(){
-        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
-    }
-
     //copying the file
     private void copyFile(InputStream in, OutputStream out) throws IOException {
         byte[] buffer = new byte[1024];
@@ -165,16 +154,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    //Request and response of user and the bot
-    public static void mainFunction (String[] args) {
-        MagicBooleans.trace_mode = false;
-        System.out.println("trace mode = " + MagicBooleans.trace_mode);
-        Graphmaster.enableShortCuts = true;
-        Timer timer = new Timer();
-        String request = "Hello.";
-        String response = chat.multisentenceRespond(request);
-
-        System.out.println("Human: "+request);
-        System.out.println("Robot: " + response);
-    }
 }
